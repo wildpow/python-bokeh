fff0ed670c48414ebd8059a4f402bcad07686eeb
Aaron Wilder <element1028@gmaill.com>
2018-06-20 10:48:43 -0700

    added graph classes

--- src/draw.py
+++ src/draw.py
@@ -5,17 +5,22 @@ from bokeh.plotting import figure
 from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
 from bokeh.palettes import Spectral8
 
+from graph import *
+
+graph_data = Graph()
+graph_data.debug_create_test_data()
+print(graph_data.vertexes)
 N = 8 
 node_indices = list(range(N))
 
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(Spectral8, 'color')
-graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
 
 graph.edge_renderer.data_source.data = dict(
     start=[0]*N,
